8c6f7c7748a6e242f1683d16b8a7ef122830c370
Jason Erasmus <jason.erasmus@live.co.za>
2024-02-17 02:04:10 +0000

    API for Blog Posts
    Added the API for the blog posts and created dict to store key:values

--- main.py
+++ main.py
@@ -1,7 +1,24 @@
 from flask import Flask, render_template
+import requests
+
 
 app = Flask(__name__)
 
+response = requests.get("https://api.npoint.io/ca9dd2cf4f4c1caebf94").json()
+blog_objects = []
+for i in response:
+    blog_obj = {
+        "id": i["id"],
+        "title": i["title"],
+        "subtitle": i["subtitle"],
+        "body": i["body"],
+        "image_url": i["image_url"],
+    }
+    blog_objects.append(blog_obj)
+
+# The line `print(blog_objects)` is printing the contents of the `blog_objects` list. It is used to
+# display the data retrieved from the API in a readable format in the console.
+print(blog_objects)
 
 @app.route("/")
 def home():
