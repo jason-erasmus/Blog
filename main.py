c2f6594ed88a002fa42ea96bb1130a1cf7b98bcb
Jason Erasmus <69726847+jason-erasmus@users.noreply.github.com>
2024-02-20 23:33:01 +0000

    Email functionality
    Added email functionality

--- main.py
+++ main.py
@@ -1,9 +1,15 @@
+import smtplib
+
 import requests
 from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
 posts = requests.get("https://api.npoint.io/ca9dd2cf4f4c1caebf94").json()
+
+
+MY_EMAIL = "useremail"
+PASSWORD = "password"
 
 
 @app.route("/")
@@ -20,10 +26,7 @@ def about():
 def contact():
     if request.method == "POST":
         data = request.form
-        print(data["name"])
-        print(data["email"])
-        print(data["phone_number"])
-        print(data["message"])
+        send_email(data["name"], data["email"], data["phone_number"], data["message"])
         return render_template("contact.html", msg_sent=True)
     return render_template("contact.html", msg_sent=False)
 
@@ -35,6 +38,16 @@ def show_post(index):
         if blog_post["id"] == index:
             requested_post = blog_post
     return render_template("post.html", post=requested_post)
+
+
+def send_email(name, email, phone, message):
+    email_message = (
+        f"Subject: New Subscriber\n\n{name}\n{email}\n{phone}\n{message}\n",
+    )
+    with smtplib.SMTP("smtp.gmail.com") as connection:
+        connection.starttls()
+        connection.login(user=MY_EMAIL, password=PASSWORD)
+        connection.sendmail(MY_EMAIL, MY_EMAIL, email_message)
 
 
 if __name__ == "__main__":
